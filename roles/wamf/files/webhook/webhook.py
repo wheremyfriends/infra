bda11ec8e3ee5c6957ead32bf6fd826c2c745371
YR <laiyr52@gmail.com>
2024-05-31 22:30:38 +0800

    build: add initial scripts to deploy production (#1)
    * Add docker role
    
    * Add nginx role
    
    * Add WAMF role
    
    * Add main playbook
    
    * Add secrets file
    
    * Add ansible hosts file
    
    * Add gitignore
    
    * Remove compose.yaml
    
    * Update README.md
    
    * Update README indentation

---
+++ roles/wamf/files/webhook/webhook.py
@@ -0,0 +1,44 @@
+import logging
+import subprocess
+
+from fastapi import FastAPI, Request
+
+from client.deploy_frontend import deploy_frontend
+
+logging.basicConfig()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+app = FastAPI()
+
+
+BACKEND_REPONAME = "wheremyfriends/webapp"
+FRONTEND_REPONAME = "wheremyfriends/nusmods"
+
+
+@app.post("/postreceive")
+async def receive_json(request: Request):
+    json_data = await request.json()
+
+    if "action" not in json_data or json_data["action"] != "completed":
+        return {"message": "JSON data received successfully"}
+
+    repo_name = json_data["repository"]["full_name"]
+
+    if repo_name == FRONTEND_REPONAME:
+        deploy_frontend(json_data)
+    elif repo_name == BACKEND_REPONAME:
+        subprocess.run(["sudo", "systemctl", "restart", "wamf-backend"])
+
+    return {"message": "JSON data received successfully"}
+
+
+def main():
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
